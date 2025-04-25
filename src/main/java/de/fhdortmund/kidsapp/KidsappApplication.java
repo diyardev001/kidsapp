f539d5edd629a6050b95b892d5d04de8b42ab99a
diyardev001 <diyarhas.dev@gmail.com>
2025-04-25 02:55:58 +0200

    feat(Backend): Projektstruktur

---
+++ src/main/java/de/fhdortmund/kidsapp/KidsappApplication.java
@@ -0,0 +1,13 @@
+package de.fhdortmund.kidsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidsappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/fhdortmund/kidsapp/KidsappApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fhdortmund.kidsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
