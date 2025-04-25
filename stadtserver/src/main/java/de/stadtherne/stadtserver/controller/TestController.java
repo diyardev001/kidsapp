a238e839d7f612cb726ad3558a76d2e6ce2ca661
diyardev001 <diyarhas.dev@gmail.com>
2025-04-25 18:59:57 +0200

    chore: Initiale Projektstruktur für Backend(Stadtserver) angelegt + feat(Backend, stadtserver): Projektnamen Ausgabe

---
+++ stadtserver/src/main/java/de/stadtherne/stadtserver/controller/TestController.java
@@ -0,0 +1,13 @@
+package de.stadtherne.stadtserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String testEndpoint() {
+        return "Stadtserver Project";
+    }
+}
