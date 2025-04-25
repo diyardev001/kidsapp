eb472b80f133580013997c18f5a1dac3d3e4bfbc
diyardev001 <diyarhas.dev@gmail.com>
2025-04-25 20:54:49 +0200

    feat(stadtServerAplication): Ausgabe zum testen

--- stadtserver/src/main/java/de/stadtherne/stadtserver/StadtserverApplication.java
+++ stadtserver/src/main/java/de/stadtherne/stadtserver/StadtserverApplication.java
@@ -9,6 +9,7 @@ public class StadtserverApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StadtserverApplication.class, args);
 		System.out.println("Projekt: " + StadtserverApplication.class.getSimpleName());
+		System.out.println("Testing Git");
 	}
 
 }
