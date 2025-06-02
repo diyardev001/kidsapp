ac5a1e89f3093e286398ae59a7ed8c6cd5155b80
luffy191919 <baturalpkalabalik99@gmail.com>
2025-06-02 14:43:47 +0200

    feat(profile): test user eingeführt für login zustand

--- mobile/app/(tabs)/profile.tsx
+++ mobile/app/(tabs)/profile.tsx
@@ -2,6 +2,13 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const profile = () => {
+  const user = {
+    isAuthenticated: true, 
+    name: "Kevin Mayer",
+    email: "Kevin_Mayer@gmail.com",
+    phone: "+49 (179) 4354233",
+    image: require('../../assets/images/profile.jpg'),
+  };
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>profile</Text>
