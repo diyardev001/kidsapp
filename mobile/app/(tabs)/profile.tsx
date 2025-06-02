e7116a0b734b9a4579902831c0ac587b8c0330e3
luffy191919 <baturalpkalabalik99@gmail.com>
2025-06-02 14:57:30 +0200

    feat(profile): unterscheidung zwischen angemeldet und nicht angemeldet

--- mobile/app/(tabs)/profile.tsx
+++ mobile/app/(tabs)/profile.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View ,Image} from 'react-native'
 import React from 'react'
 
 const profile = () => {
@@ -8,12 +8,39 @@ const profile = () => {
     email: "Kevin_Mayer@gmail.com",
     phone: "+49 (173) 4354233",
     image: require('../../assets/images/profile.jpg'),
-  };
+  }
+
+  //wenn nt eingeloggt 
+  if (!user.isAuthenticated) {
+    return (
+      <View>
+        <Text>Bitte logge dich ein, um dein Profil zu sehen</Text>
+      </View>
+    );
+  }
+
+  //wenn eingeloggt
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>profile</Text>
+    <View>
+      <Image source={user.image} />
+      <Text>{user.name}</Text>
+      <Text>{user.email}</Text>
+      <Text>{user.phone}</Text>
+
+      <TouchableOpacity>
+        <Text>Favoriten</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text>Angemeldete Aktivitäten</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text>Besuchte Aktivitäten</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text>Einstellungen</Text>
+      </TouchableOpacity>
     </View>
-  )
+  );
 }
 
 export default profile
