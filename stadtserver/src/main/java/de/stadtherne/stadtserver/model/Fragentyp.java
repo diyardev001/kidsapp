78960838e10173388a8499c7d9c84e6d2d92df5a
luffy191919 <baturalpkalabalik99@gmail.com>
2025-06-12 16:46:46 +0200

    feat(stadtserver): die Klassen Umfrage und Fragentyp implementiert

--- stadtserver/src/main/java/de/stadtherne/stadtserver/model/Fragentyp.java
+++ stadtserver/src/main/java/de/stadtherne/stadtserver/model/Fragentyp.java
@@ -1,5 +1,16 @@
 package de.stadtherne.stadtserver.model;
 
-public class Fragentyp {
+public abstract class Fragentyp {
 
+    protected String titel;
+    protected String beschreibung;
+
+    public Fragentyp(String titel, String beschreibung) {
+        this.titel = titel;
+        this.beschreibung = beschreibung;
+    }
+
+    public abstract String getTitel();
+
+    public abstract String getBeschreibung();
 }
--- stadtserver/src/main/java/de/stadtherne/stadtserver/model/Umfrage.java
+++ stadtserver/src/main/java/de/stadtherne/stadtserver/model/Umfrage.java
@@ -1,7 +1,13 @@
 package de.stadtherne.stadtserver.model;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Umfrage {
+    private List<Fragentyp> fragen = new ArrayList<>();
 
-    
+    public void add(Fragentyp frage) { fragen.add(frage); }
+    public void remove(Fragentyp frage) { fragen.remove(frage); }
+    public Fragentyp getFragentypt(int n) { return fragen.get(n); }
+    public List<Fragentyp> getFragentypen() { return fragen; }
 }
