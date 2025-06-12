ef76fc4e624e5a790e0aacad212ed7c0fd0d905d
luffy191919 <baturalpkalabalik99@gmail.com>
2025-06-12 14:26:47 +0200

    feat(stadtserver):abstrakte statusklasse

---
+++ stadtserver/src/main/java/de/stadtherne/stadtserver/model/Status.java
@@ -0,0 +1,36 @@
+package de.stadtherne.stadtserver.model;
+
+public abstract class Status {
+
+    public void trendingSetzen(Umfrage umfrage) {
+        
+    }
+
+    public void umfrageVerfuegbarSetzen(Umfrage umfrage) {
+    }
+
+    public void bewertungVerfuegbarSetzen(Umfrage umfrage) {
+        
+    }
+
+    public void abgeschlossenSetzen(Umfrage umfrage) {
+        
+    }
+
+    public void liveSetzen(Umfrage umfrage) {
+        
+    }
+
+    public void ausstehendSetzen(Umfrage umfrage) {
+        
+    }
+
+    public void stattfindendSetzen(Umfrage umfrage) {
+        
+    }
+
+    public void geloeschtSetzen(Umfrage umfrage) {
+        
+    }
+
+}
