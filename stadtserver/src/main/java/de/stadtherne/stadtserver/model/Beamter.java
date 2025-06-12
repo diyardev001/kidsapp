5e727cfe25d0202d55b5fe6f3e9c4fbd4e1fb39d
luffy191919 <baturalpkalabalik99@gmail.com>
2025-06-12 17:30:26 +0200

    feat(stadtserver):die Klasse Beamter implementiert

--- stadtserver/src/main/java/de/stadtherne/stadtserver/model/Beamter.java
+++ stadtserver/src/main/java/de/stadtherne/stadtserver/model/Beamter.java
@@ -1,5 +1,24 @@
 package de.stadtherne.stadtserver.model;
 
-public class Beamter {
+public class Beamter extends Nutzer {
 
+   
+
+    public Aktivitaet aktivitaetErstellen(String titel, String beschreibung, TerminT termin) {
+        return new Aktivitaet();
+    }
+
+    public Umfrage umfrageErstellen(String titel, String beschreibung) {
+        return new Umfrage();
+    }
+
+    public void liveSetzen(Aktivitaet a) { a.liveSetzen(); }
+    public void abgeschlossenSetzen(Aktivitaet a) { a.abgeschlossenSetzen(); }
+    public void ausstehendSetzen(Aktivitaet a) { a.ausstehendSetzen(); }
+    public void geloeschtSetzen(Aktivitaet a) { a.geloeschtSetzen(); }
+    public void stattfindendSetzen(Aktivitaet a) { a.stattfindendSetzen(); }
+    public void bewertungVerfuegbarSetzen(Aktivitaet a) { a.bewertungVerfuegbarSetzen(); }
+    public void umfrageVerfuegbarSetzen(Aktivitaet a) { a.umfrageVerfuegbarSetzen(); }
+    public void trendingSetzen(Aktivitaet a) { a.trendingSetzen(); }
 }
+
